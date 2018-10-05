3b0e708845a1323f06b7ad3f7ea04c77bb268554
Marcus Hansson <machalvan@hotmail.com>
2018-10-05 20:40:43 +0200

    Move scripts to js

--- nian/js/main.js
+++ nian/js/main.js
@@ -1,10 +1,24 @@
+const guesses = new Set(["a", "b", "c"]);
+
 window.onload = function () {
     const letters = "AMNCKASDR";
     setSquareLetters(letters);
+    addGuessListener();
 };
 
 function setSquareLetters(letters) {
-    for (var i = 0; i < letters.length; i++) {
+    for (let i = 0; i < letters.length; i++) {
         document.getElementById("square" + i).innerHTML = letters.charAt(i);
     }
 }
+
+function addGuessListener() {
+    document.getElementById("buttonGuess").addEventListener("click", function(){
+        var ul = document.getElementById("solutions");
+        var li = document.createElement("li");
+        var solution = "test";
+        li.appendChild(document.createTextNode(solution));
+        ul.appendChild(li);
+    });
+}
+
