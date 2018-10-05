621d3d20a281cfd0e459b1c44d2855ca0e2b7a2c
Robin Hübner <profan@prfn.se>
2018-10-05 20:02:07 +0200

    check for end state

--- memory/game.js
+++ memory/game.js
@@ -6,7 +6,7 @@ var ac = [
   "cat_5.jpeg",
   "cat_6.jpeg",
   "cat_7.jpeg",
-  "cat_8.jpeg"
+  "cat_8.jpeg",
   "cat_1.jpeg",
   "cat_2.jpeg",
   "cat_3.jpeg",
@@ -22,8 +22,8 @@ function create_game() {
     state : null,
     number_of_moves : 0,
     flipped_card : null,
-    flipped_cards = {},
-    board = ac
+    flipped_cards : {},
+    board : ac
   }
 }
 
@@ -31,21 +31,24 @@ function shuffle_array(arr) {
   for (var i = 0; i < 16; ++i) {
     var n_i = Math.floor((Math.random() * 16))
     var tmp = arr[n_i];
-    arr[n_i] = elem;
+    arr[n_i] = arr[i];
     arr[i] = tmp;
   }
 }
 
 function start_game(game_state) {
-  game_state.state = "time_to_move"
+  game_state.state = "time_to_flip"
   shuffle_array(game_state.board)
 }
 
 function flip_card(game_state, id) {
 
   if (game_state.state == "time_to_flip") {
+
     game_state.state = "time_to_match"
     game_state.flipped_card = game_state.board[id]
+    game_state.number_of_moves++
+
   } else if (game_state.state == "time_to_match") {
 
     if (game_state.flipped_card == game_state.board[id]) {
@@ -58,6 +61,24 @@ function flip_card(game_state, id) {
 
     game_state.number_of_moves++
 
+    var total_flipped_cards = 0
+    for (c in game_state.flipped_cards) {
+      total_flipped_cards++
+    }
+
+    if (total_flipped_cards == 8) {
+      game_state.state = "game_over"
+    } else {
+      game_state.state = "time_to_match"
+    }
+
   }
 
 }
+
+var gs = create_game()
+start_game(gs)
+flip_card(gs, 0)
+flip_card(gs, 3)
+
+console.log(JSON.stringify(gs))
